405f56977ed6df3032cef27ff0e6e2924e02ef2b
Emil Olsson <emil.olsson@edu.nacka.se>
2016-03-09 18:17:04 +0100

    Cleaning up the document
    Added comments and insurance that software notices parameters. Also,
    started to develop the frontend.

--- wp-skyltar-widget.php
+++ wp-skyltar-widget.php
@@ -11,27 +11,44 @@ License: MIT
 /**
  * Class o_skyltar_widget -> Extends WP_Widget
  * Start instance when window loads the object and fire __construct.
+ *
+ * @since 1.0
+ *
  */
 class o_skyltar_widget extends WP_Widget {
 
+  //Constructor
   function __construct() {
     parent::__construct(false, $name = __('WP Skyltar Widget'));
   }
 
+  //Function form
   function form() {
 
   }
 
+  //Function update
   function update() {
 
   }
 
+  /**
+  * Function widget
+  *
+  * @param $args Arguments
+  * @param $instance Instaces
+  */
   function widget($args, $instance) {
-
+    ?>
+      <div class="widget wp-skyltar-widget">
+        <h2>Title</h2>
+      </div>
+    <?php
   }
 
 }
 
+//Add action on load of page, initiate function register_widget
 add_action('widgets_init', function() {
   register_widget('o_skyltar_widget');
 });
